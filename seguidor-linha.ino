732902083c8cd0df644d030a5e94953557be09e9
Marcos Vinicius <marcosviniciuscl@outlook.com>
2018-05-07 08:59:59 -0300

    Alterações do Forked de Dandan

--- seguidor-linha.ino
+++ seguidor-linha.ino
@@ -1,98 +1,71 @@
 #include <AFMotor.h>
  
 AF_DCMotor motor_um(1); //Seleciona o motor 1
-AF_DCMotor motor_dois(2); //Seleciona o motor 4
-AF_DCMotor motor_tres(3); //Seleciona o motor 1
+AF_DCMotor motor_dois(2); //Seleciona o motor 2
+AF_DCMotor motor_tres(3); //Seleciona o motor 3
 AF_DCMotor motor_quatro(4); //Seleciona o motor 4
 
 #define PIN_SE A4
 #define PIN_SD A5
 
 int SENSOR_E, SENSOR_D;
- 
-//deslocamentos de calibracao
-int leftOffset = 0, rightOffset = 0, centre = 0;
-//pinos para a velocidade e direcao do motor
-int speed1 = 3, speed2 = 11, direction1 = 12, direction2 = 13;
+
 //velocidade inicial e deslocamento de rotacao
-int startSpeed = 200, rotate = 75;
-//limiar do sensor
-int threshold = 5;
+int startSpeed = 50, rotate = 40;
 //velocidades iniciais dos motores esquerdo e direito
 int left = startSpeed, right = startSpeed;
+//condição para seguir em frente
+boolean andar = true;
+//velocidades individuais de cada motor para andar em linha reta
+int motor1=50, motor2=50, motor3=70, motor4=70;
+//resistêcia maxima para cada sensor
+int limite_se = 130, limite_sd = 100; 
 
-int limite_se = 75, limite_sd = 80; 
+//retorna true caso a resistência do sensor esquerdo seja menor
+//que a resistência maxima
+boolean isBlackLeft(){
+  lerSensores();
+  return SENSOR_E <= limite_se;  
+} 
 
-//Rotina de calibracao do sensor
-void calibrate(){
- for (int x=0; x<10; x++) //Executa 10 vezes para obter uma media
- {
-   delay(100);
-   SENSOR_E = analogRead(PIN_SE);
-   SENSOR_D = analogRead(PIN_SD);
-   
-   leftOffset = leftOffset + SENSOR_E;
-   //centre = centre + SENSOR_D;
-   rightOffset = rightOffset + SENSOR_D;
-   delay(100);
- }
- //obtem a media para cada sensor
- leftOffset = leftOffset /10;
- rightOffset = rightOffset /10;
- //centre = centre / 10;
- //calcula os deslocamentos para os sensores esquerdo e direito
- //leftOffset = centre - leftOffset;
- //rightOffset = centre - rightOffset;
+//retorna true caso a resistência do sensor direito seja menor
+//que a resistência maxima
+boolean isBlackRight(){
+  lerSensores();
+  return SENSOR_D <= limite_sd;
 }
- 
+  
 void setup(){
-  //calibrate();
   Serial.begin(9600);
-  Serial.println("Fora Temer");
 }
 
 void lerSensores(){
-  SENSOR_E = analogRead(PIN_SE) + leftOffset;
-  SENSOR_D = analogRead(PIN_SD) + rightOffset;
-  Serial.print("SE: "); Serial.println(SENSOR_E);
-  Serial.print("SD: "); Serial.println(SENSOR_D);
+  SENSOR_E = analogRead(PIN_SE);
+  SENSOR_D = analogRead(PIN_SD);
 }
 
-//Essa função faz o robô virar a esquerda(vai para Cuba)
-void vireEsquerda(){
-  Serial.println("ESQUERDA");
-  left = startSpeed;
-  right = startSpeed - rotate;
-  setVelocidadeMotores();
-}
-
-//Vira o robô para a direita(Bolsonaro 2018)
-void vireDireita(){
-  Serial.println("DIREITA");
-  left = startSpeed - rotate;
-  right = startSpeed;
-  setVelocidadeMotores();
-}
-
+//virar para a esquerda parando as rodas da esquerda
 void vireEsquerdaParado(){
   left = 0;
   right = startSpeed + rotate;
   setVelocidadeMotores();
 }
 
+//virar para a direita parando as rodas das direita
 void vireDireitaParado(){
   left = startSpeed + rotate;
   right = 0;
   setVelocidadeMotores();
 }
 
+//segue em frente
 void seguirFrente(){
-  Serial.println("FRENTE");
   left = startSpeed;
   right = startSpeed;
-  setVelocidadeMotores();
+  setVelocidadeAndar();
 }
 
+//velocidade dos motores quando estão rodando
 void setVelocidadeMotores(){
   motor_um.setSpeed(left);
   motor_um.run(FORWARD);
@@ -104,21 +77,59 @@ void setVelocidadeMotores(){
   motor_tres.run(FORWARD);      
 }
 
-void loop()
-{
-  //utiliza a mesma velocidade em ambos os motores
+//velocidade dos motores quando segue em linha reta
+void setVelocidadeAndar(){
+  motor_um.setSpeed(motor1);
+  motor_um.run(FORWARD);
+  motor_quatro.setSpeed(motor4);
+  motor_quatro.run(FORWARD);
+  motor_dois.setSpeed(motor2);
+  motor_dois.run(FORWARD);
+  motor_tres.setSpeed(motor3);
+  motor_tres.run(FORWARD);      
+}
+
+//zera a velocidade dos motores
+void setVelocidadeStop(){
+  andar = false;
+  motor_um.setSpeed(0);
+  motor_um.run(BACKWARD);
+  motor_quatro.setSpeed(0);
+  motor_quatro.run(BACKWARD);
+  motor_dois.setSpeed(0);
+  motor_dois.run(FORWARD);
+  motor_tres.setSpeed(0);
+  motor_tres.run(FORWARD);     
+}
+
+void loop(){
+  //utiliza a mesma velocidade em ambos os motores para iniciar
   left = startSpeed;
   right = startSpeed;
   lerSensores();
-  
-  if(SENSOR_E <= limite_se){
-    vireEsquerda();
+  //caso os dois sensores estejam em cima da linha preta
+  if(isBlackRight() && isBlackLeft()){
+    seguirFrente();
   }
-  else if(SENSOR_D <= limite_sd){ 
-    vireDireita();
+  //caso o sensor direito esteja em cima da linha preta
+  else if(isBlackRight()){
+    setVelocidadeStop();
+   //enquanto o sensor esquerdo não encontrar a linha preta
+    while(!isBlackLeft()){
+      vireEsquerdaParado();
+      }
+    andar=true;
   }
-  else{
+  //caso o sensor esquerdo esteja em cima da linha preta  
+  else if(isBlackLeft()){
+    setVelocidadeStop();
+    //enquanto o sensor esquerdo não encontrar a linha preta
+    while(!isBlackRight()){ 
+      vireDireitaParado();
+      }
+    andar=true;
+  }
+  else if(andar){
     seguirFrente();
   }  
 }
-
